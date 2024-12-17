84cf362743e91592a17b3974a07ee1a2d968b1fb
Shiv Bhatia <shivbhatia10@gmail.com>
2024-12-17 16:59:05 +0000

    day17 part 2

--- day17/day17.ml
+++ day17/day17.ml
@@ -20,7 +20,7 @@ module IO = struct
   open Re
   open ComputerTypes
 
-  exception BadProgramFormat
+  exception BadProgramFormatException
 
   let read_file filename =
     let ch = open_in filename in
@@ -43,7 +43,7 @@ module IO = struct
       { registers = { a; b; c }
       ; program = { pointer = 0; instructions = Array.of_list program_list }
       }
-    | _ -> raise BadProgramFormat
+    | _ -> raise BadProgramFormatException
   ;;
 
   let get_computer_from_filename input_file =
@@ -59,7 +59,7 @@ module Computer = struct
   let print_computer (computer : computer) =
     let { a; b; c } = computer.registers in
     let { pointer; instructions } = computer.program in
-    Printf.printf "(A: %d, B: %d, C: %d)\n" a b c;
+    Printf.printf "\n(A: %d, B: %d, C: %d)\n" a b c;
     Printf.printf "Program: ";
     Array.iter (fun instruction -> Printf.printf "%d " instruction) instructions;
     Printf.printf "at position %d\n" pointer
@@ -77,58 +77,47 @@ module Computer = struct
     | 4 -> a
     | 5 -> b
     | 6 -> c
-    (* Should never happen in a valid program *)
     | _ -> raise InvalidComboOperand
   ;;
 
-  (* 0 *)
   let adv operand (registers : registers) : registers * int option =
     let { a; b; c } = registers in
     let combo_operand = combo operand registers in
-    let denominator = int_of_float (float_of_int 2 ** float_of_int combo_operand) in
-    { a = a / denominator; b; c }, None
+    { a = Int.shift_right a combo_operand; b; c }, None
   ;;
 
-  (* 1 *)
   let bxl operand (registers : registers) : registers * int option =
     let { a; b; c } = registers in
     let literal_operand = literal operand in
     { a; b = b lxor literal_operand; c }, None
   ;;
 
-  (* 2 *)
   let bst operand (registers : registers) : registers * int option =
     let { a; b = _; c } = registers in
     let combo_operand = combo operand registers in
     { a; b = combo_operand mod 8; c }, None
   ;;
 
-  (* 4 *)
   let bxc _operand (registers : registers) : registers * int option =
     let { a; b; c } = registers in
     { a; b = b lxor c; c }, None
   ;;
 
-  (* 5 *)
   let out operand (registers : registers) : registers * int option =
     let combo_operand = combo operand registers in
     registers, Some (combo_operand mod 8)
   ;;
 
-  (* 6 *)
   let bdv operand (registers : registers) : registers * int option =
     let { a; b = _; c } = registers in
     let combo_operand = combo operand registers in
-    let denominator = int_of_float (float_of_int 2 ** float_of_int combo_operand) in
-    { a; b = a / denominator; c }, None
+    { a; b = Int.shift_right a combo_operand; c }, None
   ;;
 
-  (* 7 *)
   let cdv operand (registers : registers) : registers * int option =
     let { a; b; c = _ } = registers in
     let combo_operand = combo operand registers in
-    let denominator = int_of_float (float_of_int 2 ** float_of_int combo_operand) in
-    { a; b; c = a / denominator }, None
+    { a; b; c = Int.shift_right a combo_operand }, None
   ;;
 
   let apply_non_jump_instruction (computer : computer) instruction : computer * int option
@@ -154,7 +143,6 @@ module Computer = struct
   ;;
 
   let maybe_step (computer : computer) : (computer * int option) option =
-    (* let () = print_computer computer in *)
     let { registers = _; program } = computer in
     let { pointer; instructions } = program in
     let len_instructions = Array.length instructions in
@@ -187,36 +175,36 @@ module Computer = struct
     let () = print_newline () in
     res
   ;;
+end
 
-  let does_a_reproduce_instructions a (program : program) : bool =
-    let init_registers = { a; b = 0; c = 0 } in
-    let init_computer = { registers = init_registers; program } in
-    let n = Array.length program.instructions in
-    let rec helper i curr_computer =
-      if i >= n
-      then true
-      else (
-        match maybe_step curr_computer with
-        (* Program terminated without reproducing output *)
-        | None -> false
-        | Some (next_computer, maybe_output) ->
-          (match maybe_output with
-           | None -> helper i next_computer
-           | Some output ->
-             if program.instructions.(i) <> output
-             then false
-             else helper (i + 1) next_computer))
-    in
-    helper 0 init_computer
+module ManualCompiler = struct
+  let step_output a =
+    let mod_eight = a mod 8 in
+    let b_eight = mod_eight lxor 4 in
+    let c_eight = Int.shift_right a (mod_eight lxor 1) in
+    b_eight lxor c_eight mod 8
   ;;
 
-  let find_smallest_a_to_reproduce_instructions (program : program) : int =
-    let rec helper i =
-      match does_a_reproduce_instructions i program with
-      | true -> i
-      | false -> helper (i + 1)
-    in
+  let range n =
+    let rec helper i = if i >= n then [] else i :: helper (i + 1) in
     helper 0
+  ;;
+
+  let get_all_solutions target_list : int list =
+    let rev_targets = List.rev target_list in
+    let rec helper prefix = function
+      | [ t ] ->
+        range 8
+        |> List.map (fun x -> prefix + x)
+        |> List.filter (fun y -> t = step_output y)
+      | t :: ts ->
+        range 8
+        |> List.filter (fun x -> t = step_output (prefix + x))
+        |> List.map (fun x -> helper (Int.shift_left (prefix + x) 3) ts)
+        |> List.concat
+      | [] -> []
+    in
+    helper 0 rev_targets
   ;;
 end
 
@@ -227,9 +215,21 @@ let input_file = "day17_input.txt"
 let computer = get_computer_from_filename input_file
 let () = print_computer computer
 let output = get_output computer
-let () = List.map string_of_int output |> String.concat "," |> print_endline
-let smallest_a_to_reproduce = find_smallest_a_to_reproduce_instructions computer.program
 
 let () =
-  Printf.printf "Smallest A to reproduce instructions: %d\n" smallest_a_to_reproduce
+  Printf.printf "Output: ";
+  List.map string_of_int output |> String.concat "," |> print_endline
+;;
+
+let sols = ManualCompiler.get_all_solutions (Array.to_list computer.program.instructions)
+let min_sol = List.fold_left Int.min Int.max_int sols
+let () = Printf.printf "Smallest solution: %d" min_sol
+
+let test_output =
+  get_output { registers = { a = min_sol; b = 0; c = 0 }; program = computer.program }
+;;
+
+let () =
+  Printf.printf "Output on smallest solution: ";
+  List.map string_of_int test_output |> String.concat "," |> print_endline
 ;;
